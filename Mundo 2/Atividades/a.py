64e101e7528a53e6ecb7765e53fdf674af88c313
Leonardo5525 <122413332+Leonardo5525@users.noreply.github.com>
2023-03-03 01:05:29 -0300

    Delete Mundo 2 directory

--- Mundo 2/Atividades/a.py
+++
@@ -1,7 +0,0 @@
-n = int(input('numero = '))
-
-import random
-lista = [1, 2, 3, 4, 5]
-print(random.choice(lista))
-
-if n == 
--- Mundo 2/Atividades/ex42.py
+++
@@ -1,4 +0,0 @@
-a = int(input('Qual o tamanho da reta = '))
-b = int(input('Qual o tamanho da reta = '))
-c = int(input('Qual o tamanho da reta = '))
-
